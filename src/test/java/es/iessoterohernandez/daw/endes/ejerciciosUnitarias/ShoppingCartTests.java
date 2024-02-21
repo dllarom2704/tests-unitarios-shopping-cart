b90e6a6db206cdaa0b8ce6ab3e5d78054f7a98b7
dllarom2704 <152259860+dllarom2704@users.noreply.github.com>
2024-02-21 19:26:41 +0100

    Add files via upload

--- src/test/java/es/iessoterohernandez/daw/endes/ejerciciosUnitarias/ShoppingCartTests.java
+++ src/test/java/es/iessoterohernandez/daw/endes/ejerciciosUnitarias/ShoppingCartTests.java
@@ -1,7 +1,6 @@
 package es.iessoterohernandez.daw.endes.ejerciciosUnitarias;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
